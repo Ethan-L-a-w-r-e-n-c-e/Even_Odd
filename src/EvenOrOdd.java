ea4647c462bc5c3ad39010a806a5ad6808b5a824
Ethan-L-a-w-r-e-n-c-e <lawrence.614730@swl.k12.oh.us>
2024-09-12 08:40:21 -0400

    actually wrote code

--- src/EvenOrOdd.java
+++ src/EvenOrOdd.java
@@ -1,5 +1,18 @@
-public class Main {
+import java.util.Scanner;
+
+public class EvenOrOdd {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int num;
+
+        System.out.println("Enter a number to determine if it is even or odd");
+        num = scan.nextInt();
+
+        if(num%2==0){
+            System.out.println("Your number is even");
+        } else {
+            System.out.println("Your number is odd");
+        }
     }
 }
